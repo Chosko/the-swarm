d03347ba280b83c89cac39294b01d8c96a0cbf33
ShenW123 <shen.w.sw@gmail.com>
2014-09-13 20:28:10 -0700

    Revert "First Iteration of AI"
    This reverts commit 199a1043105878fc6f5c9ee48bd7d211ef3f5b7a.

--- Swarm/Assets/Scripts/AI.cs
+++ Swarm/Assets/Scripts/AI.cs
@@ -1,71 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class AI : MonoBehaviour {
-	
-	int bugNumber			= 0;				// Number, in order, of bugs selected (0-4)
-	GameObject[] bugPrefab;						// Array containing bug prefabs
-	
-	
+public class AI : PlayerController {
+
 	// Use this for initialization
 	void Start () {
-		
+	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+	
 	}
-	
-	void  ChooseEnemyBugs (){
-		for(int i = 0; i < 5; i++)
-		{
-			Random random = new Random();
-			// Instantiate a random bug to each spot.
-			
-			
-			Bug tempEnemyBug = Player.bugsP2;
-			
-			tempEnemyBug.GetComponent<Bug>().bugID 			= i;
-			tempEnemyBug.GetComponent<Bug>().isP2			= 1;
-			tempEnemyBug.tag 								= "enemyBug";			// set the tag on this object to "Player Bug"
-		}
-	}
-	
-	void  ChooseEnemyActions (Vector3 new enemyPatrolPos[]){
-		Bug tempTarget;
-		for(int i = 0; i < 5; i++)
-		{
-			// Instantiate a random bug to each spot.
-			Random random = new Random();
-			int randomActionNumber = random.Next(1, 6);		// 1 = Attack, 2 = Patrol, 3 = Defend, 4+ = Attack Flag
-			Debug.Log("Random Number is: " + randomActionNumber);
-			if (randomActionNumber == 1)					// Enemy will attack
-			{
-				tempTarget = Player.bugsP1[random.Next(0,4)];
-				Player.bugsP2[i].target = tempTarget;
-			}
-			if (randomActionNumber == 2)
-			{
-				tempTarget = enemyPatrolPos[random.Next(0,enemyPatrolPos.Length)]]; //Pass Patrol Position to Target
-				Player.bugsP2[i].target = tempTarget;
-			}
-			if (randomActionNumber == 3)
-			{
-				int defendID = i;
-				Bug selectedBug;
-				while ((defendID == i) || selectedBug.target == Player.bugsP2[i])
-				{
-					defendID = random.Next(0,5);		// Choose a new ID #.  If it's still the same as i, will repeat.
-					selectedBug = Player.bugsP2[defendID];
-				}
-				Player.bugsP2[i].target = selectedBug;
-			}
-			if (randomActionNumber >= 4)
-				tempTarget = Player.flagP1;
-			Player.bugsP2[i].target = tempTarget;
-		}
-	}
-	
-	
 }
--- Swarm/Assets/Scripts/Player.cs
+++ Swarm/Assets/Scripts/Player.cs
@@ -8,12 +8,6 @@ public class Player : MonoBehaviour {  // CRINGE!  Don't really need it to be a 
 	public Color color;
 	private Command[] commands;
 
-	private GameObject[] bugPoolP1;
-	public Bug[] bugsP1;
-	
-	private GameObject[] bugPoolP2;
-	public Bug[] bugsP2;
-
 	public Player(){
 		commands = new Command[AppSettings.NUMBER_OF_BUGS];
 	}
@@ -34,28 +28,8 @@ public class Player : MonoBehaviour {  // CRINGE!  Don't really need it to be a 
 		CommandEvent(this, cea);
 	}
 
-	
 	protected virtual void Start () {
 		Debug.Log("Player Start()");
-		bugPoolP1 = new GameObject[NUMBER_OF_BUGS];
-		bugsP1 = new Bug[NUMBER_OF_BUGS];
-		
-		bugPoolP2 = new GameObject[NUMBER_OF_BUGS];
-		bugsP2 = new Bug[NUMBER_OF_BUGS];
-		
-		//  Object instantiate these fellas.
-		Vector3 _positionP1 = new Vector3(0f,0f,0f); // whatev... change this for a better layout
-		Vector3 _positionP2 = new Vector3 (0f, 20f, 0f);
-		for (int i=0; i < NUMBER_OF_BUGS; i++){
-			_positionP1.x = 2*i-1.5f;
-			bugPoolP1[i] = UnityEngine.GameObject.Instantiate(bugPf, _positionP1, Quaternion.Euler(new Vector3()) ) as GameObject;
-			bugsP1[i] = bugPoolP1[i].GetComponent<Bug>();
-			_positionP2.x = 2*i-1.5f;
-			bugPoolP2[i] = UnityEngine.GameObject.Instantiate(bugPf, _positionP2, Quaternion.Euler(new Vector3()) ) as GameObject;
-			bugsP2[i] = bugPoolP2[i].GetComponent<Bug>();
-			//			spheres[i].Init(i, _position,sphereColors[i]);
-			//			spheres[i].SphereSelectionEvent += OnSphereClicked;
-		}
 	}
 
 }

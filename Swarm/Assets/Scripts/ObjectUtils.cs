08eb44efdcba8f547d20bbc9cb87acc51b8fd56c
Max <maximiliansiu@Maximilians-Retina-MBP.local>
2014-09-13 21:02:23 -0700

    Bug properties updated

---
+++ Swarm/Assets/Scripts/ObjectUtils.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ObjectUtils
+{
+	public static Dictionary<TKey, TValue> CloneDictionary<TKey, TValue>(Dictionary<TKey, TValue> original)
+	{
+		if (original==null)
+			return null;
+		
+		Dictionary<TKey, TValue> ret = new Dictionary<TKey, TValue>(original.Count, original.Comparer);
+		foreach (KeyValuePair<TKey, TValue> entry in original)
+		{
+			ret.Add(entry.Key, (TValue) entry.Value);
+		}
+		return ret;
+	}
+	
+	public static List<TValue> CloneList<TValue>(List<TValue> original)
+	{
+		List<TValue> ret = new List<TValue>(original.Count);
+		foreach (TValue entry in original)
+		{
+			ret.Add(entry);
+		}
+		
+		return ret;
+	}
+
+}
